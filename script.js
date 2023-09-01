f781f96ee03fd2a324e1aba3a54846d8ca1aa6d6
MateWojno <mateusz.k.wojno@gmail.com>
2023-09-01 10:08:15 +0200

    Colouring divs works!

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
+    cell.addEventListener('mouseover', function(event) {
+      event.target.style.backgroundColor = 'black';
+    })
     container.appendChild(cell).className = "grid-item";
   }
 };
 
 makeRows(16, 16);
 
-const cellColor = document.querySelector(".grid-item");
-cellColor.addEventListener("mouseover",() => {
-  cellColor.style.backgroundColor = 'red';
-});  
+
